c8a12984ffb654d2619ba012af214b0353b36c52
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2024-04-04 17:07:24 +0700

    fix search

--- search/Search.php
+++ search/Search.php
@@ -3,21 +3,19 @@
 namespace Search;
 class Search
 {
-    public function search(Request $request, Model $model): View
+    public function search(string $query): array
     {
-        $query = $request->query('query');
-
         $parts = explode(' ', $query);
-        $column1 = $parts[0] ?? '';
-        $column2 = $parts[1] ?? '';
-        $column3 = $parts[2] ?? '';
+        $surname = $parts[0] ?? '';
+        $name = $parts[1] ?? '';
+        $patronymic = $parts[2] ?? '';
 
-        $filteredModels = $model->where(function($query) use ($column1, $column2, $column3) {
-            $query->where($column1, 'like', '%'.$column1.'%')
-                ->where($column2, 'like', '%'.$column2.'%')
-                ->where($column3, 'like', '%'.$column3.'%');
+        $filteredEmployees = Employee::where(function($query) use ($surname, $name, $patronymic) {
+            $query->where('surname', 'like', '%'.$surname.'%')
+                ->where('name', 'like', '%'.$name.'%')
+                ->where('patronymic', 'like', '%'.$patronymic.'%');
         })->get();
 
-        return new View('site.search', ['filteredModels' => $filteredModels]);
+        return $filteredEmployees->toArray();
     }
 }
